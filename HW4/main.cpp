43bfb350bf8388522a654b09bb89fd8abad35d40
Josh1289op <Joshuakegley@gmail.com>
2014-10-13 23:11:04 -0400

    Finished

--- HW4/main.cpp
+++ HW4/main.cpp
@@ -5,15 +5,10 @@ int* makeTT(int* scores_start, int* scores_end);
 int main(){
 
 
-	int array[6] = { 5, 32, 49, 2, 1, 12 };
+	int array[5] = { 1, 2, 3, 4, 5 };
 
-	std::cout << &array[0] - &array[5] << std::endl;
+	std::cout << makeTT(&array[0], &array[4])[0];
 
-
-	std::cout << makeTT(&array[0], &array[5])[10];
-
-	int five;
-	std::cin >> five;
 	return 0;
 }
 
@@ -22,21 +17,14 @@ int main(){
 int* makeTT(int* scores_start, int* scores_end){
 	int scoreSize = abs(scores_start - scores_end) + 1;
 	int size = (abs(scores_start - scores_end) + 1) * 2 - 1;
-	std::cout << size << std::endl << std::endl;
 	int * array = new int[size];
 
 	for (int i = 0; i < scoreSize; ++i){
 		array[(size - scoreSize) + i] = scores_start[i];
 	}
 
-	for (int i = 0; i < size; ++i){
-		std::cout << array[i] << std::endl;
+	for (int i = (size - scoreSize) - 1; i >= 0; --i){
+		array[i] = array[i * 2 + 1] > array[i * 2 + 2] ? array[i * 2 + 1] : array[i * 2 + 2];
 	}
-
-
-
-
-	std::cout << std::endl;
-
 	return array;
 }
